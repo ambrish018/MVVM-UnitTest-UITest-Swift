6a29ba9c497a59377300dc31bde47bec64536522
ambrish018 <incredible.bil@gmail.com>
2019-09-17 10:54:52 +0530

    code signing added

--- MovieAppUITests/MovieAppUITests.swift
+++ MovieAppUITests/MovieAppUITests.swift
@@ -9,17 +9,23 @@
 import XCTest
 
 class MovieAppUITests: XCTestCase {
-
+    var app : XCUIApplication!
     override func setUp() {
+
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+         app = XCUIApplication()
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app.launch()
+        
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        app.launchArguments.append("--uitesting")
+
     }
 
     override func tearDown() {
@@ -27,12 +33,13 @@ class MovieAppUITests: XCTestCase {
     }
 
     func testExample() {
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
                                         
     }
     func testScrollTableViewWithPagination(){
-        let app = XCUIApplication()
+        //XCTAssertTrue(app.isDisplayingTableScroll)
         let tableView = app.tables["tableViewIdentifier"]
         tableView.swipeUp()
         tableView.swipeUp()
@@ -43,6 +50,12 @@ class MovieAppUITests: XCTestCase {
         tableView.swipeUp()
         tableView.swipeUp()
         tableView.swipeUp()
+        XCTAssertFalse(app.isDisplayingTableScroll)
 
+    }
+}
+extension XCUIApplication {
+    var isDisplayingTableScroll : Bool {
+        return otherElements["tableViewIdentifier"].exists
     }
 }
